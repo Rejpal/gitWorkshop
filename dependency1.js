f601e7286a85e8d7cf0a41b82765b2b32ae1292b
Roman Nikrmajer <roman.nikrmajer@brandembassy.com>
2018-04-30 17:20:24 +0200

    fix

--- dependency1.js
+++ dependency1.js
@@ -5,12 +5,9 @@ addEventListenerasdda
 screenasda
 sda
 addEventListenerasd
-<<<<<<< HEAD
 
 daw
 wadwadwadwa
 dawdaw
-=======
 aslkdhasd
 laskdhaskldh
->>>>>>> 6769e215e1774305cddd0dd2e410eea5fa59912b
